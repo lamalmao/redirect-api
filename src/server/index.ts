bcc74d75243a16a569332766d5f5a67eb6f578da
lamalmao <jelamasus@gmail.com>
2023-03-23 15:28:30 +0300

    Update index.ts
    logger moved to EventEmitter

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,6 +8,7 @@ import mainRouter from '../routers/main-router.js';
 import logger from '../logger.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { EventEmitter } from 'stream';
 
 const app = express();
 
@@ -18,9 +19,9 @@ const options: {
 const server = https.createServer(
   {
     key: fs.readFileSync(path.join(PROCESS_DIR, options.key)).toString(),
-    cert: fs.readFileSync(path.join(PROCESS_DIR, options.cert)).toString(),
+    cert: fs.readFileSync(path.join(PROCESS_DIR, options.cert)).toString()
   },
-  app,
+  app
 );
 
 app.use(cors());
@@ -28,14 +29,50 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(logger);
 
+declare global {
+  let debugLoggerConnections: Map<string, [string, string, string, number]>;
+  let debugLoggerEventEmitter: EventEmitter;
+}
+
 if (MODE === 'DEVELOPMENT') {
-  app.use(async (req, _, next) => {
+  debugLoggerEventEmitter = new EventEmitter();
+  debugLoggerConnections = new Map();
+  app.use(async (req, res, next) => {
     const then = Date.now();
-    await next();
-    const delay = Date.now() - then;
-    req.log.debug(req.body, `${req.url}\t${req.method} request from ${req.ip}\t| ${delay}ms`);
+    const connectionId = crypto.randomUUID();
+    res.locals['connectionId'] = connectionId;
+    debugLoggerConnections.set(connectionId, [req.url, req.method, req.ip, then]);
+    next();
+  });
+
+  debugLoggerEventEmitter.on('connectionClosed', (connectionId: string) => {
+    try {
+      const connectionDebugData = debugLoggerConnections.get(connectionId);
+      if (!connectionDebugData) {
+        return;
+      }
+
+      console.log(
+        `${connectionDebugData[1]} request to ${connectionDebugData[0]} from ${connectionDebugData[2]}\tprocessed for ${connectionDebugData[3]}ms`
+      );
+    } catch (e) {}
   });
 }
 
+app.use((_, res, next) => {
+  res.locals.answer = {
+    code: 200,
+    body: {}
+  };
+
+  next();
+});
 app.use('/api', mainRouter);
+app.use((_, res) => {
+  res.statusCode = res.locals.answer.code;
+  res.json(res.locals.answer.body);
+
+  debugLoggerEventEmitter?.emit('connectionClosed', res.locals.connectionId);
+});
+
 export default server;
